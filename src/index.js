2e73a16d6df0d17c4dd4f162352698c68a0e42c8
Andrea Monterrosa <54961047+Arrocito82@users.noreply.github.com>
2021-10-11 13:50:26 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+//Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+//It should display a h1 heading.
+
+//It should display an unordered list (bullet points).
+ReactDOM.render(
+  <div>
+    <h1>Hi there!</h1>
+    <ul>
+      <li>I spoke in chinesse for a minute</li>
+      <li>I made the longest English exam in my life, 45 minutes long</li>
+      <li>
+        I have started working on Web DEvelopment Bootcamp once and for all
+      </li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
+//It should contain 3 list elements.
