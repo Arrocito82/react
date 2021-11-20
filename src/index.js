b4d02e97b7518cdb98905d9bf9c99d33cdb7b93f
Andrea Monterrosa <54961047+Arrocito82@users.noreply.github.com>
2021-11-20 11:31:19 -0600

    inline style

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+// using lorem picsum
 const img = "https://picsum.photos/200";
 
+// looks like a python dictionary to me, and
+// It must be like this in order to work propertly
+const inlineStyle = {
+  color: "purple",
+  fontFamily: "sans-serif"
+};
 ReactDOM.render(
   <div>
-    <h1 className="heading" spellCheck="true" contentEditable="true">
-      My Favourite Foods
-    </h1>
+    <h1 style={inlineStyle}>My Favourite Foods</h1>
     <div>
       <img src={img} alt="ramdom" />
       <img src={img + "?grayscale"} alt="ramdom in grayscale" />
